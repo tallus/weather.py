5165a8abdf96eabe0a2c18f6475a29609417f143
Paul Munday <contactme@paulmunday.net>
2016-03-31 16:38:43 -0700

    Added README and license

--- weather.py
+++ weather.py
@@ -166,6 +166,24 @@ Valid report types:
     Temperature Extremes in XML (XTE)
     Observations in XML (XOB)
     Zone Forecast (ZFP)
+
+Copyright (C) 2016, Paul Munday.
+
+PO Box 28228, Portland, OR, USA 97228
+paul at paulmunday.net
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import argparse
 from datetime import datetime
